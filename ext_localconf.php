6fbfb6b36416d1f079c7e5fb9415a345e0b6c00c
Falk Gebauer <fgebauer@3pc.de>
2017-06-13 17:07:16 +0200

    get the fully qualified class name from ::class

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,6 @@
 defined('TYPO3_MODE') or die('');
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/backend.php']['constructPostProcess'][] =
-    'Aoe\\Becookies\\Hook\\BackendHook->process';
+    \Aoe\Becookies\Hook\BackendHook::class . '->process';
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/index_ts.php']['preprocessRequest'][] =
-    'Aoe\\Becookies\\Hook\\FrontendHook->process';
+    \Aoe\Becookies\Hook\FrontendHook::class . '->process';
